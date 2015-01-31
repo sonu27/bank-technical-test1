f0164e1900bb89ee948afefc54e6ccf8a57546a3
Amarjeet Singh Rai <jatt27@gmail.com>
2015-01-31 14:43:33 +0000

    Add function to make test pass

--- src/AR/Bank/HSBC.php
+++ src/AR/Bank/HSBC.php
@@ -4,10 +4,20 @@ namespace AR\Bank;
 
 class HSBC implements BankInterface
 {
+    private $cardNumberLengthWithDashes = 19;
+    private $unmaskedLengths = [0, 1];
 
     public function getMaskedCardNumber($number)
     {
 
+        $numberArray = str_split($number);
+        foreach ($numberArray as $k => $v) {
+            if (!in_array($k, $this->unmaskedLengths) && is_numeric($v)) {
+                $numberArray[$k] = 'x';
+            }
+        }
+
+        return implode('',$numberArray);
     }
 
 }
